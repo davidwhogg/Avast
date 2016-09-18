5a3106095fe42cd07656b20573b432d884ec1b6a
davidwhogg <david.hogg@nyu.edu>
2016-09-18 10:49:20 -0400

    make fake data

--- code/gaussian_basis.py
+++ code/gaussian_basis.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class OneDGaussianModel():
 
-    def __init__(self, K, h, offset):
+    def __init__(self, K, h, offset, baseline=0.):
         assert K > 1
         assert h > 0.
         self.K = K
@@ -14,6 +14,7 @@ class OneDGaussianModel():
         self.means = np.arange(K) * h + offset
         self.var = (1.5 * h) ** 2 # well sampled
         self.offset = offset
+        self.baseline = baseline
         self.amps = None
 
     def _OneDGaussians(self, xs):
@@ -29,23 +30,53 @@ class OneDGaussianModel():
     def evaluate(self, xs):
         N = len(xs)
         assert xs.shape == (N, )
-        return np.sum(self._OneDGaussians(xs), axis=1)
+        return np.sum(self._OneDGaussians(xs), axis=1) + self.baseline
 
     def evaluate_derivative(self, xs):
         N = len(xs)
         assert xs.shape == (N, )
         return np.sum(self._d_OneDGaussians_dx(xs), axis=1)
 
+def make_fake_data():
+    np.random.seed(17)
+    N = 50 # number of spectra
+    K = 1000  # number of control points
+    h = 1. # spacing of control points (Ang)
+    offset = 4500. # location of k=0 control point (Ang)
+    c = 3.e8 # m / s
+    truth = OneDGaussianModel(K, h, offset, 1.0)
+    truth.set_amplitudes(-0.5 * np.random.uniform(size=K) ** 32.) # made up
+    times = (np.random.uniform(size=N) - 0.5) * 1400. # four+ years
+    rvs = 2.2 * np.sin(2. * np.pi * times / 55.81) # m / s
+    waves = 1. * truth.means # copy
+    data = np.zeros((N, len(waves)))
+    for n in range(N):
+        data[n, :] = truth.evaluate(waves) - (waves / c) * truth.evaluate_derivative(waves) * rvs[n]
+    data += 0.01 * np.random.normal(size=data.shape) # s/n = 100
+    return data, waves
+
 if __name__ == "__main__":
     import pylab as plt
 
-    K = 2 ** 16  # number of points
+    data, waves = make_fake_data()
+    N, M = data.shape
+    plt.clf()
+    n = 0
+    plt.step(waves, data[n, :], color="k", where="mid")
+    plt.xlabel("wavelength (Angstrom)")
+    plt.ylabel("normalized flux")
+    plt.savefig("data00.png")
+
+if False:
+    np.random.seed(42)
+
+    K = 2 ** 10  # number of points
     h = 0.443 # spacing of points
     offset = 1.022 # location of k=0
     model = OneDGaussianModel(K, h, offset)
     model.set_amplitudes(np.random.normal(size=K))
 
-    xs = np.arange(-5., 20., 0.01)
+    xs = np.arange(-5., 100., 0.01)
     plt.clf()
     plt.plot(xs, model.evaluate(xs), "k-")
     plt.plot(xs, model.evaluate_derivative(xs), "r-")
@@ -58,3 +89,11 @@ if __name__ == "__main__":
     plt.plot(xs, model.evaluate_derivative(xs), "r-")
     plt.axhline(0., color="r", alpha=0.25)
     plt.savefig("test2.png")
+
+    model.set_amplitudes(-0.5 * np.random.uniform(size=K) ** 32.) # made up
+    model.baseline = 1.
+    plt.clf()
+    plt.plot(xs, model.evaluate(xs), "k-")
+    plt.plot(xs, model.evaluate_derivative(xs), "r-")
+    plt.axhline(0., color="r", alpha=0.25)
+    plt.savefig("test3.png")
