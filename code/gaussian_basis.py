df88183ec8ec6d31a32342027857b03df0ebe7ce
davidwhogg <david.hogg@nyu.edu>
2016-09-28 05:37:45 -0400

    experiments

--- code/gaussian_basis.py
+++ code/gaussian_basis.py
@@ -3,6 +3,7 @@ This file is part of the Avast project.
 Copyright 2016 David W. Hogg (NYU).
 """
 import numpy as np
+c = 3.e8 # m / s
 
 class OneDGaussianModel():
 
@@ -37,46 +38,46 @@ class OneDGaussianModel():
         assert xs.shape == (N, )
         return np.sum(self._d_OneDGaussians_dx(xs), axis=1)
 
+    def evaluate_at_redshift(self, xs, rv):
+        restxs = redshift_wavelengths(xs, -rv)
+        return self.evaluate(restxs)
+
+    def evaluate_at_redshift_linearized(self, xs, rv):
+        return self.evaluate(xs) - xs * self.evaluate_derivative(xs) * (rv / c)
+
+def redshift_wavelengths(waves, v):
+    beta = v / c
+    return waves * np.sqrt((1. + beta) / (1. - beta))
+
 def make_fake_data():
     np.random.seed(17)
     N = 50 # number of spectra
     K = 1000  # number of control points
     h = 1. # spacing of control points (Ang)
     offset = 4500. # location of k=0 control point (Ang)
-    c = 3.e8 # m / s
     truth = OneDGaussianModel(K, h, offset, 1.0)
     truth.set_amplitudes(-0.5 * np.random.uniform(size=K) ** 32.) # made up
     times = (np.random.uniform(size=N) - 0.5) * 1400. # four+ years
-    rvs = 2.2 * np.sin(2. * np.pi * times / 55.81) # m / s
+    rvs = 2200. * np.sin(2. * np.pi * times / 55.81) # m / s
     waves = 1. * truth.means # copy
     data = np.zeros((N, len(waves)))
     for n in range(N):
-        data[n, :] = truth.evaluate(waves) - (waves / c) * truth.evaluate_derivative(waves) * rvs[n]
+        restwaves = redshift_wavelengths(waves, -rvs[n])
+        data[n, :] = truth.evaluate(restwaves)
     data += 0.01 * np.random.normal(size=data.shape) # s/n = 100
     return data, waves
 
 if __name__ == "__main__":
     import pylab as plt
-
-    data, waves = make_fake_data()
-    N, M = data.shape
-    plt.clf()
-    n = 0
-    plt.step(waves, data[n, :], color="k", where="mid")
-    plt.xlabel("wavelength (Angstrom)")
-    plt.ylabel("normalized flux")
-    plt.savefig("data00.png")
-
-if False:
     np.random.seed(42)
 
     K = 2 ** 10  # number of points
     h = 0.443 # spacing of points
-    offset = 1.022 # location of k=0
+    offset = 4999. # location of k=0
     model = OneDGaussianModel(K, h, offset)
     model.set_amplitudes(np.random.normal(size=K))
 
-    xs = np.arange(-5., 100., 0.01)
+    xs = np.arange(5000, 5010., 0.01)
     plt.clf()
     plt.plot(xs, model.evaluate(xs), "k-")
     plt.plot(xs, model.evaluate_derivative(xs), "r-")
@@ -95,5 +96,26 @@ if False:
     plt.clf()
     plt.plot(xs, model.evaluate(xs), "k-")
     plt.plot(xs, model.evaluate_derivative(xs), "r-")
+    delta = 0.01 * np.sqrt(model.var)
+    numerical_derivative = (model.evaluate(xs + delta) - model.evaluate(xs)) / delta
+    plt.plot(xs, numerical_derivative, "g--", alpha=0.5)
     plt.axhline(0., color="r", alpha=0.25)
     plt.savefig("test3.png")
+
+    plt.clf()
+    plt.plot(xs, model.evaluate(xs), "k-")
+    rv = 1.e4 # m/s
+    plt.plot(xs, model.evaluate_at_redshift(xs, rv), "r-")
+    plt.plot(xs, model.evaluate_at_redshift_linearized(xs, rv), "g--", alpha=0.5)
+    plt.savefig("test4.png")
+
+if False:    
+    data, waves = make_fake_data()
+    N, M = data.shape
+    n = 0
+    plt.clf()
+    plt.step(waves, data[n, :], color="k", where="mid")
+    plt.xlabel("wavelength (Angstrom)")
+    plt.ylabel("normalized flux")
+    plt.savefig("data00.png")
+
