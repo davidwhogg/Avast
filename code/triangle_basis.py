3c1fba749568ea38ea01abc588cc094baafcce92
megbedell <megbedell@gmail.com>
2016-12-03 18:51:30 -0600

    switched back to leastsq

--- code/triangle_basis.py
+++ code/triangle_basis.py
@@ -4,46 +4,16 @@ Copyright 2016 Megan Bedell (Chicago).
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from lmfit import minimize, Parameters # Hogg doesn't like lmfit
-c = 2.99792458e8 # precise enough?
+from scipy.optimize import leastsq
+c = 2.99792458e8
 
-def make_pars(ams0, scales0, vs0):
-    # given starting guesses, create a Parameters() object for fitting
-    pars = Parameters()
-    for i,am in enumerate(ams0):
-        pars.add('ams'+str(i), value=am)
-    for i,scale in enumerate(scales0):
-        pars.add('scales'+str(i), value=scale)
-    for i,v in enumerate(vs0):
-        pars.add('vs'+str(i), value=v)
-    return pars
-    
 def unpack_pars(pars, n_ms, n_epoch):
     # unpack Parameters() object
-    ams = []
-    scales = []
-    vs = []
-    for i in range(n_ms):
-        name = 'ams'+str(i)
-        ams = np.append(ams, pars[name])
-    for i in range(n_epoch):
-        name = 'scales'+str(i)
-        scales = np.append(scales, pars[name])
-    for i in range(n_epoch):
-        name = 'vs'+str(i)
-        vs = np.append(vs, pars[name])
+    ams = pars[0:n_ms]
+    scales = pars[n_ms:n_ms+n_epoch]
+    vs = pars[n_ms+n_epoch:]
     return ams, scales, vs
     
-def change_par_status(pars, name, vary=False):
-    # change the fitting status of a given parameter. fixes the parameter by default.
-    if not name in ['ams','scales','vs']:
-        print("{0} is not a valid parameter.".format(name))
-        return pars
-    for par_key in pars.iterkeys():
-        if name in par_key:
-            pars[par_key].vary = vary
-    return pars
-
 def triangle(xs, xms, del_x):
     # returns values of triangle components centered on xms at location xs
     # xs & xms must be broadcastable
@@ -101,36 +71,35 @@ if __name__ == "__main__":
     ams0 = np.random.normal(size=n_ms) + np.median(ys[0])
     scales0 = np.ones(n_epoch)
     vs0 = np.random.normal(size=n_epoch) * 5.0
-    pars0 = make_pars(ams0, scales0, vs0)
-    pars0 = change_par_status(pars0, 'vs', vary=False)  # fix the velocities
-    soln = minimize(min_function, pars0, args=fa)
-    # second fit with velocities free:
-    pars1 = soln.params
-    pars1 = change_par_status(pars1, 'vs', vary=True)
-    soln = minimize(min_function, pars1, args=fa)
+    pars0 = np.append(ams0, np.append(scales0, vs0))
+    ftol = 1.49012e-08  # default is 1.49012e-08
+    soln = leastsq(min_function, pars0, args=fa, ftol=ftol)
+
     # look at the fit:
-    pars = soln.params
+    pars = soln[0]
     ams, scales, vs = unpack_pars(pars, n_ms, n_epoch)
     
-    '''''
+
     for e in range(n_epoch):
         calc = model(xs[e], xms, del_x, ams * scales[e])
         x_plot = np.linspace(lnwave[0],lnwave[-1],num=1000)
         calc_plot = model(x_plot, xms, del_x, ams * scales[e])
         show_plot(xs[e], ys[e], calc, x_plot, calc_plot)
-    '''
-        
-    # plotting objective function with starting RV for Hogg:
-    pars2 = soln.params
-    star_v1 = np.linspace(-20.,20.,500)
+
+     
+    # plotting objective function with leastsq tolerance:
     obj = []  # objective function
-    for v in star_v1:
-        pars2['vs0'].value = v
-        resids = min_function(pars2, xs, ys, xms, del_x)
+    all_ftol = []
+    for i in range(10):
+        soln = leastsq(min_function, pars, args=fa, ftol=ftol)
+        resids = min_function(soln[0], xs, ys, xms, del_x)
         obj = np.append(obj, np.dot(resids,resids))
+        ftol = ftol/10.  # tighten tolerance
+        all_ftol = np.append(all_ftol, ftol)
     plt.clf()
-    plt.plot(star_v1,obj)
-    plt.xlabel('starting RV guess (first epoch, m/s)')
+    plt.plot(all_ftol,obj)
+    plt.xscale('log')
+    plt.xlabel('ftol')
     plt.ylabel('objective function')
-    plt.savefig('objectivefn.png')
+    plt.savefig('objectivefn_ftol.png')
 
