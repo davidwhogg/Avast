255dd4e3aac9fd84637bf6307610ad801d75b5a0
megbedell <megbedell@gmail.com>
2017-01-11 16:57:51 -0600

    fitting multiple epochs at once - needs more work

--- code/spectral_chunk.py
+++ code/spectral_chunk.py
@@ -22,9 +22,9 @@ def read_spec(spec_file):
     index = np.arange(n_wave, dtype=np.float64)
     wave = crval1 + index*cdelt1
     spec = sp[0].data
-    obj = header['OBJECT']
-    bjd = header['ESO DRS BJD']
-    return wave, spec, obj, bjd
+    #obj = header['OBJECT']
+    #bjd = header['ESO DRS BJD']
+    return wave, spec
 
 def solve_chunk(pars0, xs, ys, yerrs, xms, del_x):
     # given starting guess of pars0, solve for best-fit pars in a chunk.
@@ -34,44 +34,33 @@ def solve_chunk(pars0, xs, ys, yerrs, xms, del_x):
         method='BFGS', jac=triangle_basis.obj_deriv)
     return res
 
-def read_chunk(spec_file, start_wave, save_dir):
-    # for a given 1D spectrum file at a given chunk, return the saved solution.
-    sp = fits.open(spec_file)
-    header = sp[0].header
-    obj = header['OBJECT']
-    bjd = header['ESO DRS BJD']
-    save_name = save_dir+'{0}_d{1}_w{2:4}.p'.format(obj,str(bjd),start_wave)
-    xs, ys, yerrs, res, obj, bjd = pickle.load(open(save_name, "rb"))
-    return xs, ys, yerrs, res, obj, bjd
-    
-def solve_spec(spec_file, number, save_dir):
-    # reads in a HARPS s1d FITS file and solves it chunk-by-chunk.
+
+def solve_all(all_xs, all_ys, save_dir):
+    # takes a list of HARPS spectra and solves them chunk-by-chunk.
     # output is saved as a series of pickle files under path save_dir.
-    print "Solving spectrum {0}...".format(spec_file)
-    wave, spec, obj, bjd = read_spec(spec_file)
     
     del_x = 1.3e-5/2.0
     
-    #for mid_ind in range(500, len(wave)-500, 500):
+    #for mid_ind in range(500, len(all_xs[0])-500, 500):
     for mid_ind in range(277320, 277320+2000, 500): # for testing
         pad = 100 # number of points to overlap with neighboring chunk
         start_ind, stop_ind = mid_ind - 250 - pad, mid_ind + 250 + pad
-        print "Solving chunk from {0:4.1f} A to {1:4.1f} A...".format(wave[start_ind], wave[stop_ind])
-        start_wave = wave[start_ind]
-        xs = [np.log(wave[start_ind:stop_ind])]
-        ys = [spec[start_ind:stop_ind]]
+        print "Solving chunk from {0:4.1f} A to {1:4.1f} A...".format(all_xs[0,start_ind], all_xs[0,stop_ind])
+        start_wave = np.exp(all_xs[0,start_ind])
+        xs = all_xs[:,start_ind:stop_ind]
+        ys = all_ys[:,start_ind:stop_ind]
         yerrs = np.sqrt(ys) # TODO: fix this
         
         xms = np.arange(np.min(xs) - 0.5 * del_x, np.max(xs) + 0.99 * del_x, del_x)
         n_ms = len(xms)
         ams0 = np.ones(n_ms)
         scales0 = [np.median(s) * scale_scale for s in ys]
-        xis0 = np.zeros(1) * xi_scale
+        xis0 = np.zeros(len(xs)) * xi_scale
         pars0 = np.append(ams0, np.append(scales0, xis0))
         
         res = solve_chunk(pars0, xs, ys, yerrs, xms, del_x)
-        save_obj = (xs, ys, yerrs, res, obj, bjd)
-        save_name = save_dir+'{0}_e{1}_w{2:04}.p'.format(obj,number,int(start_wave))
+        save_obj = (xs, ys, yerrs, xms, res)
+        save_name = save_dir+'{0}_w{2:04}.p'.format('test',int(start_wave)) # TODO: add metadata
         print "Saving chunk as {0}...".format(save_name)
         pickle.dump(save_obj, open(save_name, "wb"))
         
@@ -79,8 +68,42 @@ def solve_spec(spec_file, number, save_dir):
 if __name__ == "__main__":
     data_dir = '../data/chunk_test/'    
     filelist = glob.glob(data_dir+'*_s1d_A.fits')
-    for i,f in enumerate(filelist): 
-        if i>2:
+    #nfile = len(filelist)
+    nfile = 3 # for testing
+    all_xs = None
+  
+    for e,fn in enumerate(filelist):
+        wave, spec = read_spec(fn)
+        if all_xs is None:
+            nwave = len(wave)
+            all_xs, all_ys = np.zeros([nfile,nwave]), np.zeros([nfile,nwave])
+        assert len(wave) == nwave # THIS DOESN'T WORK - not all spectra have the same nwave!
+        if e>2:
             continue  # for testing
-        solve_spec(filelist[0], i, data_dir)
+        all_xs[e] = np.log(wave)
+        all_ys[e] = spec
+        
+    solve_all(xs, ys, data_dir)
+        
+    for p in glob.glob(data_dir+'*.p'):
+        xs, ys, yerrs, xms, res = pickle.load(open(p, "rb"))
+        pars = res['x']
+        n_epoch = len(xs)
+        n_ms = len(xms)
+        del_x = xms[1] - xms[0]
+        ams, scales, xis = triangle_basis.unpack_pars(pars, n_ms, n_epoch)
+        calcs = np.zeros((n_epoch, len(xs[e])))
+        for e in range(n_epoch):
+            xprimes = xs[e] + xis[e]
+            calc = triangle_basis.f(xprimes, xms, del_x, ams * scales[e])
+            x_plot = np.linspace(xprimes[0],xprimes[-1],num=5000)
+            calc_plot = triangle_basis.f(x_plot+xis[e], xms, del_x, ams * scales[e])
+            resid = triangle_basis.resid_function(pars, xs, ys, yerrs, xms, del_x)
+            n_x = len(xs[e])
+            resid = resid[e*n_x:(e+1)*n_x] * yerrs[e]
+            triangle_basis.save_plot(xs[e], ys[e], calc, resid, x_plot, calc_plot, 
+                'fig/wave{0:04}_epoch{1}.pdf'.format(int(np.exp(xs[e,0])),str(e)), e)
+            calcs[e] = calc
+        
+
     
